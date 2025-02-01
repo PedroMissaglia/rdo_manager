6e8f0cd94804b7e18cabcb98fe4fc65d133e7f77
PedroMissaglia <pedro.missaglia@gmail.com>
2025-02-01 17:10:53 -0300

    att

--- src/app/job/new/new.component.ts
+++ src/app/job/new/new.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-new',
-    imports: [],
     templateUrl: './new.component.html',
     styleUrl: './new.component.scss'
 })
