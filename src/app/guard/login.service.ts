a96542aea17438e8278c4c3b06e6c21f8a7f3d0c
PedroMissaglia <pedro.missaglia@gmail.com>
2025-04-07 23:32:24 -0300

    ok

---
+++ src/app/guard/login.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuard implements CanActivate {
+
+  // Aqui, você pode substituir pelo seu método de verificação de autenticação
+  private isLoggedIn: boolean = false; // Defina sua variável de controle (ex.: a partir de um serviço)
+
+  constructor(
+    private router: Router,
+    private userService: UserService) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+
+
+  if(this.userService.isLoggedIn()) {
+    this.router.navigate(['/home']);
+    return false;
+    } else {
+      // Redireciona para a página de login ou qualquer outra página desejada
+
+      return true;
+    }
+  }
+}
